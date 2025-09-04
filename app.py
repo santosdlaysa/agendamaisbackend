d9ac43db84580e9a4aafd6e873c62579b44800d8
santosdlaysa <santosdlaysa@gmail.com>
2025-09-04 14:59:28 -0400

    Alterações commitadas:   - ajuste no cors

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def create_app():
         'http://localhost:5173',
         'http://172.29.16.1:3001',
         'https://agendamais-x19j-32f6fx9yd-santosdlaysas-projects.vercel.app',
-        ' localhost:3002',
+        'https://agendamais-x19j.vercel.app'
+        'http://localhost:3002',
         os.getenv('FRONTEND_URL')
     ]
 
